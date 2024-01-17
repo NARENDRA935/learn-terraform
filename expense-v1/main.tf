1d32b0efbeb34255211615c4ae02fcab921221ea
t.narendra <narendratarra935@gmail.com>
2024-01-17 19:33:10 +0530

    add a code

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-oeda81a962cebfc79"]
+  vpc_security_group_ids = ["sg-0eda81a962cebfc79"]
 
   tags = {
     Name = "frontend-dev"
@@ -11,7 +11,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "backend" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-oeda81a962cebfc79"]
+  vpc_security_group_ids = ["sg-0eda81a962cebfc79"]
 
   tags = {
     Name = "backend-dev"
@@ -21,7 +21,7 @@ resource "aws_instance" "backend" {
 resource "aws_instance" "mysql" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-oeda81a962cebfc79"]
+  vpc_security_group_ids = ["sg-0eda81a962cebfc79"]
 
   tags = {
     Name = "mysql-dev"
