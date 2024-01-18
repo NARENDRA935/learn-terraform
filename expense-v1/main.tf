18cec150b9ef9df9d025b5b194fbcea2ffa3fd08
t.narendra <narendratarra935@gmail.com>
2024-01-18 11:19:24 +0530

    add a code

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "frontend" {
-  ami           = "ami-0f3c7d07486cad139"
-  instance_type = "t3.micro"
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0eda81a962cebfc79"]
 
   tags = {
