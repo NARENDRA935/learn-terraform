6c12f814ce8a3aa2ee83413e942fb01286b565a0
t.narendra <narendratarra935@gmail.com>
2024-01-18 14:41:10 +0530

    add a code

---
+++ expense-v2/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = ["sg-0eda81a962cebfc79"]
+}
+
+variable "zone_id" {
+  default = "Z055598033M3BHHZMYMN6"
+}
