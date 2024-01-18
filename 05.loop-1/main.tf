59b42748487cc9fc0e52fe7a239b46bf19c838bb
t.narendra <narendratarra935@gmail.com>
2024-01-18 16:46:01 +0530

    add a code

--- 05.loop-1/main.tf
+++ 05.loop-1/main.tf
@@ -5,3 +5,14 @@ resource "null_resource" "test" {
 output "test" {
   value = null_resource.test.*.id
 }
+
+resource "aws_instance" "test" {
+  count                  = 3
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0eda81a962cebfc79"]
+
+  tags = {
+    Name = "test"
+  }
+}
